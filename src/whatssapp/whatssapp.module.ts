85383fc849efac62cd1ff33363bcff91aba6be0d
Gilson-chipombo <fernandochipombo@gmail.com>
2026-03-28 12:00:24 +0100

    feat: init implement webhook whatssapp module

--- src/whatssapp/whatssapp.module.ts
+++ src/whatssapp/whatssapp.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { WhatsappService } from './whatssapp.service';
 import { WhatsappController } from './whatssapp.controller';
+import { WebhookModule } from './webhook/webhook.module';
 
 @Module({
   providers: [WhatsappService],
-  controllers: [WhatsappController]
+  controllers: [WhatsappController],
+  imports: [WebhookModule]
 })
 export class WhatssappModule {}
